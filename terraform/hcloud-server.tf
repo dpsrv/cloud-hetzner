6d3b3a954a02a23ca39e5bdf248309c5dc64dd64
Max <max@maxf.net>
2024-01-05 17:13:16 -0500

    updated

--- terraform/hcloud-server.tf
+++ terraform/hcloud-server.tf
@@ -1,9 +1,13 @@
+data "template_file" "user_data" {
+  template = file("user_data")
+}
+
 resource "hcloud_server" "dpsrv" {
   name        = "dpsrv"
   image       = "fedora-39"
   server_type = var.HCLOUD_SERVER_TYPE
   location    = data.hcloud_location.current.name
   ssh_keys    = [hcloud_ssh_key.default.id]
-  user_data   = file("user_data")
+  user_data   = data.template_file.user_data.rendered
 }
 
