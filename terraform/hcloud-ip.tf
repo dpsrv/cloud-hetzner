1afb61aa6993193e62a0bc0378eabe4a498e0a0c
Max <max@maxf.net>
2024-01-05 16:02:49 -0500

    updated

--- terraform/hcloud-ip.tf
+++ terraform/hcloud-ip.tf
@@ -8,3 +8,9 @@ resource "hcloud_floating_ip_assignment" "dpsrv" {
   server_id      = hcloud_server.dpsrv.id
 }
 
+resource "hcloud_rdns" "dpsrv" {
+  floating_ip_id = hcloud_floating_ip.dpsrv.id
+  ip_address     = hcloud_floating_ip.dpsrv.ip_address
+  dns_ptr        = "hc-ash-1.dpsrv.me"
+}
+
