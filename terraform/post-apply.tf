9736a0a092ee53d6dcb2ebfeff834f007ae1c08c
Max <max@maxf.net>
2024-01-06 16:46:56 -0500

    updated

--- terraform/post-apply.tf
+++ terraform/post-apply.tf
@@ -5,7 +5,10 @@ resource "terraform_data" "post-apply" {
 
   provisioner "local-exec" {
     command = <<-EOT
-		ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@${hcloud_server.dpsrv.ipv4_address} "mkdir -p ~/.config/git"
+		while ! ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@${hcloud_server.dpsrv.ipv4_address} "mkdir -p ~/.config/git"; do
+			echo "Waiting for the server to become available"
+			sleep 2
+		done
 		scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no ~/.config/git/openssl-* root@${hcloud_server.dpsrv.ipv4_address}:.config/git/
 	EOT
   }
