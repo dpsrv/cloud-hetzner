f4a54754413bd69e5cd1afc7ccb628465e81509f
Max <max@maxf.net>
2024-01-05 22:15:53 -0500

    updated

--- terraform/hcloud-server.tf
+++ terraform/hcloud-server.tf
@@ -2,6 +2,7 @@ data "template_file" "user_data" {
   template = file("user_data")
   vars = {
     hcloud_volume_data_id = hcloud_volume.dpsrv-data.id
+    hcloud_ssh_key_dpsrv = hcloud_ssh_key.dpsrv.public_key
   }
 }
 
@@ -10,7 +11,7 @@ resource "hcloud_server" "dpsrv" {
   image       = "fedora-39"
   server_type = var.HCLOUD_SERVER_TYPE
   location    = data.hcloud_location.current.name
-  ssh_keys    = [hcloud_ssh_key.default.id]
+  ssh_keys    = [hcloud_ssh_key.dpsrv.id]
   user_data   = data.template_file.user_data.rendered
 }
 
--- terraform/hcloud-ssh.tf
+++ terraform/hcloud-ssh.tf
@@ -1,4 +1,4 @@
-resource "hcloud_ssh_key" "default" {
+resource "hcloud_ssh_key" "dpsrv" {
   name       = "dpsrv"
   public_key = file("~/.ssh/id_rsa.pub")
 }
