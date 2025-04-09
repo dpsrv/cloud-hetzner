0a174034a206582814664e5c055dfe20bd09de5e
Max <max@maxf.net>
2025-04-09 13:37:05 -0400

    updated

--- terraform/hcloud-server.tf
+++ terraform/hcloud-server.tf
@@ -2,6 +2,8 @@ data "template_file" "user_data" {
   count    = var.HCLOUD_SERVER_COUNT
   template = file("user_data")
   vars = {
+    hcloud_location       = data.hcloud_location.current.name
+    hcloud_node_id        = count.index
     hcloud_volume_data_id = hcloud_volume.dpsrv-data[count.index].id
     hcloud_ssh_key_dpsrv  = hcloud_ssh_key.dpsrv.public_key
   }
@@ -9,7 +11,7 @@ data "template_file" "user_data" {
 
 resource "hcloud_server" "dpsrv" {
   count        = var.HCLOUD_SERVER_COUNT
-  name         = "hc-ash-${count.index}"
+  name         = "hc-${data.hcloud_location.current.name}-${count.index + 1}"
   image        = "fedora-39"
   server_type  = var.HCLOUD_SERVER_TYPE
   location     = data.hcloud_location.current.name
